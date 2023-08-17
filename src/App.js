a8c1b03a5946c8698d9c9b4fef284557a05ea5da
N Kavindya <110715995+NKavindya@users.noreply.github.com>
2023-08-17 22:09:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CustomTable from './components/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Table App</h1>
+      <CustomTable />
+    </div>
+  );
+}
+
+export default App;
